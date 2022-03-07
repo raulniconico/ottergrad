56adff7de296308ec5f63d36a670b30ef7da416d
raulniconico <raulniconico@outlook.com>
2022-03-07 12:51:10 +0100

    add module construction file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Ottergrad",
-    version="1.0.4",
+    version="0.0.4",
     author="Zixing QIU",
     author_email="zixing.qiu@etu.enseeiht.fr",
     description="Ottergrad is an automatic differentiation tool support plenty of NumPy functions who borns from Nuwa "
